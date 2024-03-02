34070d0f4e1d3e45891972ba54b7165c3bfd3d20
Jonathan Waters <jonathan@waters.to>
2024-03-02 13:25:25 -0500

    changes to login page

--- src/createUser.js
+++ src/createUser.js
@@ -1,7 +1,20 @@
+import fs from 'fs';
+import qs from 'querystring';
+
 function createUser() {
 
 }
 
-export function serveCreatePage() {
-
+export function createPageServe(req, res) {
+	res.end(fs.readFileSync('src/app/create.html'))
+}
+export function createPageHandler(req, res) {
+	let data = '';
+	req.on('data', (d)=>{
+		data += d.toString();
+	})
+	req.on('end', ()=>{
+		console.log(qs.parse(data));
+		res.end("Success");
+	})
 }
