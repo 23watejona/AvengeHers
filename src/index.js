00bbe12e63f7ada79947201de405c5db23020cf1
Jonathan Waters <jonathan@waters.to>
2024-03-02 15:16:37 -0500

    reorg

--- src/index.js
+++ src/index.js
@@ -5,8 +5,13 @@ import {createPageHandler, createPageServe} from './createUser.js'
 const app = express();
 const port = 3000;
 
+app.use(express.static('src/app/public'));
+
 app.get('/', (req, res) => {
-  res.end(fs.readFileSync('src/app/index.html'))
+  res.end(fs.readFileSync('src/app/public/index.html'))
+})
+app.get('/', (req, res) => {
+  res.end(fs.readFileSync('src/app/public/index.html'))
 })
 app.get('/create.html', createPageServe);
 app.post('/create.html', createPageHandler);
