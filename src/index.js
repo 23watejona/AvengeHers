4a23e6db763d4d604ef31774ff758da2a0b44538
Jonathan Waters <jonathan@waters.to>
2024-03-02 11:32:43 -0500

    Add index page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import fs from 'fs';
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(fs.readFileSync('app/index.html'))
 })
 
 app.listen(port, () => {
